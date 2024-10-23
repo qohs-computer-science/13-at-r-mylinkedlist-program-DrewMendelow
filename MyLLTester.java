6596114c1ea66e49a61f6531e5c44f75344d633d
DrewMendelow <103079010+DrewMendelow@users.noreply.github.com>
2024-10-23 17:27:54 +0000

    work for 10/23

--- MyLLTester.java
+++ MyLLTester.java
@@ -1,10 +1,70 @@
-public class MyLLTester.java
+//Drew Mendelow, pd 7, 10/23/24
+//Tester file for MyLinkedList class
+public class MyLLTester
 {
   public static void main(String [] args)
   {
     MyLinkedList list = new MyLinkedList();
-    list.add("hi");
-    System.out.println(list.toString());
+    System.out.println("Original list: \n" + list.toString());
+    list.add("one");
+    System.out.println("Item added. New list: \n" + list.toString());
+    list.add("two");
+    System.out.println("Item added. New list: \n" + list.toString());
+    list.add("three");
+    System.out.println("Item added. New list: \n" + list.toString());
+    list.add("four");
+    System.out.println("Item added. New list: \n" + list.toString());
+    list.add("five");
+    System.out.println("Item added. New list: \n" + list.toString());
+    list.addFirst("six");
+    System.out.println("Item added at beginning. New list: \n" + list.toString());
+    list.addLast("seven");
+    System.out.println("Item added at end. New list: \n" + list.toString());
+    list.addFirst("eight");
+    System.out.println("Item added at beginning. New list: \n" + list.toString());
+    list.addFirst("nine");
+    System.out.println("Item added at beginning. New list: \n" + list.toString());
+    list.addFirst("ten");
+    System.out.println("Item added at beginning. New list: \n" + list.toString());
+
+    list.set(2, "eleven");
+    System.out.println("Item changed. New list: \n" + list.toString());
+
+    System.out.println("item 3: " + list.get(2) + "\n");
+
+    list.remove(7);
+    System.out.println("Item removed. New list: \n" + list.toString());
+    list.removeFirst();
+    System.out.println("First item removed. New list: \n" + list.toString());
+    list.removeLast();
+    System.out.println("Last item removed. New list: \n" + list.toString());
+
+    System.out.println("List size:" + list.size() + "\n");
+
+    System.out.println("List empty? " + list.isEmpty());
+
+    list.removeLast();
+    System.out.println("Last item removed. New list: \n" + list.toString());
+    list.removeLast();
+    System.out.println("Last item removed. New list: \n" + list.toString());
+    list.removeLast();
+    System.out.println("Last item removed. New list: \n" + list.toString());
+    list.removeLast();
+    System.out.println("Last item removed. New list: \n" + list.toString());
+    list.removeLast();
+    System.out.println("Last item removed. New list: \n" + list.toString());
+    list.removeLast();
+    System.out.println("Last item removed. New list: \n" + list.toString());
+    list.removeLast();
+    System.out.println("Last item removed. New list: \n" + list.toString());
+    list.removeLast();
+    System.out.println("Last item removed. New list: \n" + list.toString());
+
+    list.remove(100);
+    System.out.println("Item removed. New list: \n" + list.toString());
+
+    System.out.println("List size:" + list.size() + "\n");
+
+    System.out.println("List empty? " + list.isEmpty());
   } // end main
-  
 } // end class
--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,9 +1,11 @@
+//Drew Mendelow, pd 7, 10/23/24
+//MyLinkedList class with methods to add remove and change items 
 public class MyLinkedList
 {
     public ListNode head;
     public MyLinkedList(){
         head = null;
-    }
+    }//end MyLinkedList
 
     // returns the current # of ListNode elements in the LinkedList
     public int size(){
@@ -12,45 +14,50 @@ public class MyLinkedList
         while (temp != null){
             count++;
             temp = temp.getNext();
-        }
+        }//end while
         return count;
-    }
+    }//end size
     
     // returns true if there are no ListNode elements in the LinkedList
     public boolean isEmpty(){
         return head == null;
-    }
+    }//end isEmpty
 
     // adds a ListNode element to the end of the LinkedList, always returns true.
     public boolean add(Object obj){
-        ListNode temp = head;
-        while (temp.getNext() != null){
-            temp = temp.getNext();
-        }
-        temp.setNext(new ListNode(obj, null));
+        if (head == null){
+            head = new ListNode(obj, null);
+        }//end if
+        else{
+            ListNode temp = head;
+            while (temp.getNext() != null){
+                temp = temp.getNext();
+            }//end while
+            temp.setNext(new ListNode(obj, null));
+        }//end else
         return true;
-    }
+    }//end add
 
     // adds a ListNode element to the beginning of the LinkedList
     public boolean addFirst(Object obj){
         if (head == null){
             head = new ListNode(obj, null);
-        }
+        }//end if
         else{
             head = new ListNode(obj, head);
-        }
+        }//end else
         return true;
-    }
+    }//end addFirst
 
     // adds a ListNode element to the end of the LinkedList
     public boolean addLast(Object obj){
         ListNode temp = head;
         while (temp.getNext() != null){
             temp = temp.getNext();
-        }
+        }//end while
         temp.setNext(new ListNode(obj, null));
         return true;
-    }
+    }//end addLast
 
     // changes the information stored as the value of the ListNode element at the given position
     public Object set(int i, Object obj){
@@ -62,12 +69,12 @@ public class MyLinkedList
                 old = temp.getValue();
                 temp.setValue(obj);
                 break;
-            }
+            }//end if
             count++;
             temp = temp.getNext();
-        }
+        }//end while
         return old;
-    }
+    }//end set
 
     // returns the information stored as the value of the ListNode element at a given position
     public Object get(int i){
@@ -76,21 +83,24 @@ public class MyLinkedList
         while (temp != null){
             if (count == i){
                 return temp.getValue();
-            }
+            }//end if
             count++;
             temp = temp.getNext();
-        }
+        }//end while
         return null;
-    }
+    }//end get
 
     // removes the ListNode element at the given position leaving the rest of the LinkedList intact
     public Object remove(int i){
+        if (i > size() - 1){
+            return null;
+        }//end if
         Object val = new Object();
         if (i == 0){
             val = head.getValue();
             head = head.getNext();
             return val;
-        }
+        }//end if
         ListNode temp = head.getNext();
         ListNode tempOld = head;
         int count = 1;
@@ -99,13 +109,13 @@ public class MyLinkedList
                 val = temp.getValue();
                 tempOld.setNext(temp.getNext());
                 return val;
-            }
+            }//end if
             count++;
-            temp = temp.getNext();
             tempOld = temp;
-        }
+            temp = temp.getNext();
+        }//end while
         return null;
-    }
+    }//end remove
 
     // removes the ListNode element at the beginning of the LinkedList and returns the value of the node
     public Object removeFirst(){
@@ -113,17 +123,17 @@ public class MyLinkedList
         val = head.getValue();
         head = head.getNext();
         return val;
-    }
+    }//end removeFirst
 
     // removes the ListNode element at the end of the LinkedList and returns the value of the node
     public Object removeLast(){
         if (head == null){
             return null;
-        }
+        }//end if
         else if (head.getNext() == null){
             head = null;
             return null;
-        }
+        }//end else if
         else {
             ListNode temp = head;
             Object val = new Object();
@@ -133,8 +143,8 @@ public class MyLinkedList
             val = temp.getNext().getValue();
             temp.setNext(null);
             return val;
-        }
-    }
+        }//end else
+    }//end removeLast
 
     // returns a String representation of the LinkedList in the following format: 0:Hello
     public String toString(){
@@ -145,7 +155,7 @@ public class MyLinkedList
             result += count + ":" + temp.getValue() + "\n";
             count++;
             temp = temp.getNext();
-        }
+        }//end while
         return result;
-    }
-}
+    }//end toString
+}//end MyLinkedList
